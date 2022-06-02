53c45756ef7b431a621452cef8f91902da54a658
Tigran <tigran.demerchyan@gmail.com>
2022-06-02 13:43:03 +0400

    change runScheduler value to String type

--- src/main/java/com/tigran/spring6pmmyExpectedPrice/controller/EmailScheduler.java
+++ src/main/java/com/tigran/spring6pmmyExpectedPrice/controller/EmailScheduler.java
@@ -1,5 +1,6 @@
 package com.tigran.spring6pmmyExpectedPrice.controller;
 
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,18 +9,18 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 @Service
-public class scheduler {
+public class EmailScheduler {
 
     @Autowired
     private PriceDetailController priceDetailController;
 
     @Value("run.scheduler")
-    private boolean runScheduler;
+    private String runScheduler;
 
     @Scheduled(fixedRate = 120_000)
     public void sendEmail() throws IOException {
-
-        if (!runScheduler) {
+        boolean toBoolean = Boolean.parseBoolean(runScheduler);
+        if (!toBoolean) {
             return;
         }
 
