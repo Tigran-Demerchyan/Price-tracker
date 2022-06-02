5407e0d87261282be8bd3888c2aac3b3baa97d34
Tigran <tigran.demerchyan@gmail.com>
2022-06-02 14:43:09 +0400

    change table name to price_detail

--- src/main/java/com/tigran/spring6pmmyExpectedPrice/entity/PriceDetail.java
+++ src/main/java/com/tigran/spring6pmmyExpectedPrice/entity/PriceDetail.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "my_price")
+@Table(name = "price_detail")
 @Data
 @NoArgsConstructor
 public class PriceDetail {
---
+++ src/main/resources/db/migration/V1_1__change_table_name.sql
@@ -0,0 +1,1 @@
+ALTER TABLE public.my_price RENAME TO price_detail;
