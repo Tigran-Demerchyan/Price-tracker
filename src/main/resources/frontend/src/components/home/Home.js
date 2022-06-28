80ed3aa9741d0044339b424825eb207c05f58bb4
Anna Zakaryan <anahit.zakaryan1992@gmail.com>
2022-06-29 00:21:37 +0400

    fix styles

--- src/main/resources/frontend/src/components/home/Home.js
+++ src/main/resources/frontend/src/components/home/Home.js
@@ -4,12 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addProductAsync, fetchProductByEmailAsync, selectPage, deleteProductAsync, changePage } from '../../features/product/productSlice';
 
 import { Link } from "react-router-dom";
-import styles from './Home.module.css';
 import SwiperSlider from '../swiperSlider';
 import { Pagination } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Footer from '../footer/index';
+import LogoPm from '../../assets/img/logoPm.png';
+import LogoRalphLauren from '../../assets/img/logoRalphLauren.png';
+import ZapposLogo from '../../assets/img/zappos.png';
+import Hugoboss from '../../assets/img/hugoboss.png';
+import styles from './Home.module.css';
+
+import '../../App.css';
 
 const Home = () => {
 
@@ -58,74 +64,83 @@ const Home = () => {
 
     <Fragment>
       <div className={styles.banner}>
-        <h1 className={styles.title}>TRACK 6PM ZAPPOS HUGO BOSS PRICES FOR FREE!</h1>
-        <p className={styles.subTitle}>We track prices and availability of 6pm, Zappos, Hugo Boss products for you! Get the best price and save money by utilizing
-          <a className={styles.link} href='https://www.6pm.com/' target="_blank">6pm.com</a>
-          <a className={styles.link} href='https://www.zappos.com/' target="_blank">Zappos.com</a>
-          <a className={styles.link} href='https://www.hugoboss.com/us/men/' target="_blank">Hugo Boss.com</a>
-        </p>
+        <h1 className={styles.title}>TRACK PRICES FOR FREE!</h1>
+        <p className={styles.subTitle}>We track prices and availability of products for you! Get the best price and save money by utilizing</p>
+        <div className={styles.linkGroups}>
+          <a className={styles.link} href='https://www.6pm.com/' target="_blank"><img className={styles.logo} src={LogoPm} /></a>
+          <a className={styles.link} href='https://www.zappos.com/' target="_blank"><img className={styles.logo} src={ZapposLogo} /></a>
+          <a className={styles.link} href='https://www.hugoboss.com/us/men/' target="_blank"><img className={styles.logo} src={Hugoboss} /></a>
+          <a className={styles.link} href='https://www.ralphlauren.com/' target="_blank"><img className={styles.logo} src={LogoRalphLauren} /></a>
+        </div>
         <div className={styles.homePage}>
           <input className={styles.inputSearch} type="text" placeholder='Search' onChange={e => setEmail(e.target.value)} />
           <button className={styles.btnSearch} onClick={search}><i className="fa fa-search" aria-hidden="true"></i></button>
         </div>
       </div>
-      <Container maxWidth="xl">
-        <table>
-          <thead>
-            <tr>
-              <th>Price</th>
-              <th>Email</th>
-              <th>Link</th>
-              <th>History</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          {products.map(e => (
-            <tbody key={e.id}>
+      <div className={styles.backgroundColor}>
+        <Container maxWidth="xl">
+          <table>
+            <thead>
               <tr>
-                <td data-column="price" className={styles.price}>{e.price}</td>
-                <td data-column="clientEmail">{e.clientEmail}</td>
-                <a className={styles.link} href={e.url} target="_blank" ><td data-column="url" >{e.url}</td></a>
-                <td data-column="history"><Link className={styles.link} to={'/history/' + e.id}>history</Link></td>
-                <td data-column="history"><button onClick={() => handDelete(e.id)} className={styles.btnDelete}>Delete</button></td>
+                <th>Price</th>
+                <th>Email</th>
+                <th>Link</th>
+                <th>History</th>
+                <th>Delete</th>
               </tr>
-            </tbody>
-          ))}
-        </table>
+            </thead>
 
-        <div>
-          <p className={styles.elements}>all elements {pageOfProd.totalElements}</p>
-          <Pagination color="primary" count={pageOfProd.totalPages} rowsPerPage={10} onChange={handleChange} />
-        </div>
+            {products.map(e => (
+              <tbody key={e.id}>
+                <tr>
+                  <td data-column="price" className={styles.price}>{e.price}</td>
+                  <td data-column="clientEmail">{e.clientEmail}</td>
+                  <a className={styles.url} href={e.url} target="_blank"><td className={styles.urlTable} data-column="url" >{e.url}</td></a>
+                  <td data-column="history"><Link className={styles.link} to={'/history/' + e.id}>history</Link></td>
+                  <td data-column="history" className={styles.iconDelete} onClick={() => handDelete(e.id)}><i className="fa fa-trash-o"></i></td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
 
-        <div className={styles.homePageConatiner}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} lg={6}>
-              <div className={styles.blockItem}>
-                <p className={styles.InputTable}>Link</p>
-                <input className={styles.input} type="text" onChange={e => setLink(e.target.value)} />
-              </div>
-              <div className={styles.blockItem}>
-                <p className={styles.InputTable}> Email </p>
-                <input className={styles.input} type="text" onChange={e => setNewEmail(e.target.value)} />
-              </div>
-              <div className={styles.blockItem}>
-                <p className={styles.InputTable}> Price</p>
-                <input className={styles.input} type="text" onChange={e => setPrice(e.target.value)} />
-              </div>
-              <button className={styles.button} onClick={addNewProduct}>Add new</button>
+          <div>
+            <p className={styles.elements}>all elements {pageOfProd.totalElements}</p>
+            <Pagination color="primary" count={pageOfProd.totalPages} rowsPerPage={10} onChange={handleChange} />
+          </div>
+
+          <div className={styles.homePageConatiner}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} lg={6}>
+                <div className={styles.blockItem}>
+                  <p className={styles.InputTable}>Link</p>
+                  <input className={styles.input} type="text" onChange={e => setLink(e.target.value)} />
+                </div>
+                <div className={styles.blockItem}>
+                  <p className={styles.InputTable}> Email </p>
+                  <input className={styles.input} type="text" onChange={e => setNewEmail(e.target.value)} />
+                </div>
+                <div className={styles.blockItem}>
+                  <p className={styles.InputTable}> Price</p>
+                  <input className={styles.input} type="text" onChange={e => setPrice(e.target.value)} />
+                </div>
+                <button className={styles.button} onClick={addNewProduct}>Add new</button>
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                <h1>TRACK PRICES FOR FREE!</h1>
+                <p>We track prices and availability of products for you! Get the best price and save money by utilizing</p>
+              </Grid>
             </Grid>
-            <Grid item xs={12} lg={6}>
-              <h1>TRACK 6PM ZAPPOS HUGO BOSS PRICES FOR FREE!</h1>
-              <p>We track prices and availability of 6pm, Zappos, Hugo Boss products for you! Get the best price and save money by utilizing</p>
-            </Grid>
-          </Grid>
-        </div>
+          </div>
+        </Container>
+      </div>
+      <div className={styles.backgBlackColor}>
+      <Container maxWidth="xl">
         <div className={styles.slider}>
           <SwiperSlider />
         </div>
       </Container>
-     <Footer/>
+      </div>
+      <Footer />
     </Fragment>
   );
 }
--- src/main/resources/frontend/src/components/swiperSlider/index.js
+++ src/main/resources/frontend/src/components/swiperSlider/index.js
@@ -5,12 +5,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import ImgPm from '../../assets/img/6pmImg.png';
+import ImgHugoboss from '../../assets/img/hugoboss.png';
+import ImgZappos from '../../assets/img/zapposImg.png';
+import ImgRalphLauren from '../../assets/img/logoRalphLauren.png';
 
 import "./styles.css";
 
-import {  Navigation, Autoplay } from "swiper";
+import { Navigation, Autoplay } from "swiper";
 
-const SwiperSlider = () =>  {
+const SwiperSlider = () => {
   return (
     <>
       <Swiper
@@ -19,56 +23,38 @@ const SwiperSlider = () =>  {
         slidesPerGroup={4}
         loop={true}
         loopFillGroupWithBlank={true}
-       
+
         autoplay={{
-            delay: 1500,
-            disableOnInteraction: false,
-          }}
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
         navigation={false}
-        modules={[ Navigation, Autoplay ]}
+        modules={[Navigation, Autoplay]}
         className="mySwiper"
       >
         <SwiperSlide>
-            <img className="sliderImage" src="https://m.media-amazon.com/images/G/01/6pm/promos/220505/tryptic-dresses.jpg"/>
-            <p>UP TO 50% OFF MSRP!</p>
-            <a  className="sliderLink" href="https://www.6pm.com/dresses/CKvXARDE1wHiAgIBAg.zso?s=isNew%2Fdesc%2FgoLiveDate%2Fdesc%2FrecentSalesStyle%2Fdesc%2F&si=5740654%2C5847356%2C6039206%2C6004872%2C6039207%2C6015476%2C5761676%2C5823576%2C5406267%2C5387716%2C6015482%2C5761688%2C5761671%2C5445687%2C5373169%2C5410601%2C5483983&sy=1&pf_rd_r=A4B756E3910041D6B792&pf_rd_p=1b8ececd-cebb-40b9-a3c5-fed1f2fe1bf3" target="_blank"> Shop Radiant Summer Dresses</a>
+          <a href='https://www.6pm.com/' target="_blank">
+            <img className="sliderImage" src={ImgPm} />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-            <img className="sliderImage" src="https://m.media-amazon.com/images/I/61vabhkkeBL._AC_SR736,920_.jpg"/>
-            <p>Free People</p>
-            <a className="sliderLink" href="https://www.zappos.com/women-dresses/CKvXARDE1wHAAQHiAgMBAhg.zso" target="_blank"> Gia Long Vest</a>
+          <a href='https://www.hugoboss.com/us/men/' target="_blank">
+            <img className="sliderImage" src={ImgHugoboss} />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-            <img className="sliderImage" src="https://images.hugoboss.com/is/image/boss/hbeu50486233_001_300?$large$=&fit=crop,1&align=1,1&wid=462&qlt=80&fmt=webp"/>
-            <p>BOSS X KHABY RELAXED-FIT T-SHIRT IN MERCERIZED COTTON</p>
-            <a className="sliderLink" href="https://www.zappos.com/" target="_blank">Zappos</a>   
+          <a href='https://www.ralphlauren.com/' target="_blank">
+            <img className="sliderImage" src={ImgRalphLauren} />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-            <img className="sliderImage" src="https://m.media-amazon.com/images/I/813F44P2jSL._AC_SR736,920_.jpg"/>
-            <p>Lost + Wander</p>
-            <a className="sliderLink" href="https://www.6pm.com/" target="_blank"> Southern Bell Maxi Dress</a>  
+          <a href='https://www.zappos.com/' target="_blank">
+            <img className="sliderImage" src={ImgZappos} />
+          </a>
         </SwiperSlide>
-        <SwiperSlide>
-            <img className="sliderImage" src="https://m.media-amazon.com/images/G/01/6pm/promos/220505/tryptic-dresses.jpg"/>
-            <p>UP TO 50% OFF MSRP!</p>
-            <a className="sliderLink"  href="https://www.6pm.com/dresses/CKvXARDE1wHiAgIBAg.zso?s=isNew%2Fdesc%2FgoLiveDate%2Fdesc%2FrecentSalesStyle%2Fdesc%2F&si=5740654%2C5847356%2C6039206%2C6004872%2C6039207%2C6015476%2C5761676%2C5823576%2C5406267%2C5387716%2C6015482%2C5761688%2C5761671%2C5445687%2C5373169%2C5410601%2C5483983&sy=1&pf_rd_r=A4B756E3910041D6B792&pf_rd_p=1b8ececd-cebb-40b9-a3c5-fed1f2fe1bf3" target="_blank"> Shop Radiant Summer Dresses</a>
-        </SwiperSlide>
-        <SwiperSlide>
-            <img className="sliderImage" src="https://m.media-amazon.com/images/I/61vabhkkeBL._AC_SR736,920_.jpg"/>
-            <p>Free People</p>
-            <a className="sliderLink" href="https://www.zappos.com/women-dresses/CKvXARDE1wHAAQHiAgMBAhg.zso" target="_blank"> Gia Long Vest</a>
-        </SwiperSlide>
-        <SwiperSlide>
-            <img className="sliderImage" src="https://images.hugoboss.com/is/image/boss/hbeu50486233_001_300?$large$=&fit=crop,1&align=1,1&wid=462&qlt=80&fmt=webp"/>
-            <p>BOSS X KHABY RELAXED-FIT T-SHIRT IN MERCERIZED COTTON</p>
-            <a  className="sliderLink" href="https://www.zappos.com/" target="_blank">Zappos</a>  
-        </SwiperSlide>
-        <SwiperSlide>
-            <img className="sliderImage" src="https://m.media-amazon.com/images/I/813F44P2jSL._AC_SR736,920_.jpg"/>
-            <p>Lost + Wander</p>
-            <a  className="sliderLink" href="https://www.6pm.com/" target="_blank"> Southern Bell Maxi Dress</a>  
-        </SwiperSlide>
+
       </Swiper>
+
     </>
   );
 }
